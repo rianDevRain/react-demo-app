d00d53e951442b8d69b37a2e88a1b02532c80709
Rian <mail@rian.dev>
2021-01-21 16:06:08 -0600

    react-demo-initial

---
+++ src/components/welcome/welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Welcome(props) {
+    return(
+        <div className="Welcome">
+          <h1>  Welcome, {props.name}</h1>
+        </div>
+    );
+}
+
+export default Welcome
