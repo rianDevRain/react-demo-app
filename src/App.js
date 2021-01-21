cecd883722ef2a0525eaedae14a35354ca0b893d
Rian <mail@rian.dev>
2021-01-21 16:06:51 -0600

    react-demo-initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Welcome from './components/welcome/welcome';
+import Clock from './components/clock/clock';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          
+
+          <Welcome name="Rian" />
+          <h2>Welcome to React</h2>
+
+          <Clock />
+
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
+
 
 export default App;
